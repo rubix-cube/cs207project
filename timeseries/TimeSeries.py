ccb280b4949d94be506fc6441cdffa59ad407c17
Peilin Duan <duan@g.harvard.edu>
2016-10-11 01:00:48 -0400

    edit TimeSeries.py

--- timeseries/TimeSeries.py
+++ timeseries/TimeSeries.py
@@ -1,18 +1,34 @@
+import reprlib
+
 class TimeSeries:
-    def __init__(self):
 
-    def __len__(self):
+	def __init__(self, input_series):
+		self._timeseries = input_series
 
-    def __getitem__(self, index):
+	def __len__(self):
+		return len(self._timeseries)
 
-    def __setitem__(self, index, value)
+	def __getitem__(self, index):
+		if isinstance(index, slice):
+			return TimeSeries(self._timeseries[index])
+		return self._timeseries[index]
 
-    def __repr__(self):
+	def __setitem__(self, index, value):
+		self._timeseries[index] = value
 
-    def __str__(self):
+	def __repr__(self):
+		return 'TimeS({})'.format([i for i in self._timeseries])
 
-    def __iter__(self):
+	def __str__(self):
+		if len(self._timeseries) > 100:
+			return 'TimeSeries(['+', '.join('{}'.format(i) for i in self._timeseries[:10])+'...'+', '.join('{}'.format(i) for i in self._timeseries[-10:])\
+					+ ' -- omitting {} objects'.format(len(self._timeseries) - 20)
+		return 'TimeSe({})'.format([i for i in self._timeseries]) 	
 
-    def itertimes(self):
+	def __iter__(self):
+		for item in self._timeseries:
+			yield item
 
+	def itertimes(self):
+		pass
 
