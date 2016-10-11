c92f95d0369a9bbacea2a3826b72a81608570c37
Zelong Qiu <zelong430@gmail.com>
2016-10-10 23:57:34 -0400

    folder creater, function signiture created

---
+++ timeseries/TimeSeries.py
@@ -0,0 +1,18 @@
+class TimeSeries:
+    def __init__(self):
+
+    def __len__(self):
+
+    def __getitem__(self, index):
+
+    def __setitem__(self, index, value)
+
+    def __repr__(self):
+
+    def __str__(self):
+
+    def __iter__(self):
+
+    def itertimes(self):
+
+
