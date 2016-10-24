64a0e0dfa97ee17ff7738cdf36e2b5eeea83eefe
Zelong Qiu <zelong430@gmail.com>
2016-10-23 20:51:38 -0400

    debug ArrayList class

--- timeseries/ArrayTimeSeries.py
+++ timeseries/ArrayTimeSeries.py
@@ -1,20 +1,19 @@
 import numpy as np
-import TimeSeries as ts
+import collections
+from TimeSeries import TimeSeries 
+class ArrayTimeSeries(TimeSeries):
 
-class ArrayTimeSeries(ts):
-    def __init__(self, input_value, input_time ):
+    def __init__(self, input_value, input_time = None):
         if not isinstance(input_value, collections.Sequence):
             raise TypeError("Argument input_value must be Python sequence ")
-    if input_time:
         if not isinstance(input_time, collections.Sequence):
             raise TypeError("Argument input_time must be Python sequence ")
         if len(input_time) != len(input_value):
             raise ValueError("Argument input_value must have same length with input_time")
         self._time = np.array([input_time])
-    self._value = np.array([input_value])
-    self._timeseries = np.array(zip(self._time, self_value))
-    self._dict = dict(zip(self._time), range(0, len(self._time)))
-
+        self._value = np.array([input_value])
+        self._timeseries = np.array(zip(self._time, self_value))
+        #self._dict = dict(zip(self._time), range(0, len(self._time)))
 
 
     def __getitem__(self, index):
